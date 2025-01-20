3ae17ffb5169a8853dd53df06ddd9b08237178bb
PaulPPS632 <jeffertsap@hotmail.com>
2025-01-20 02:51:48 -0500

    corregido merge

--- src/controllers/User/userController.ts
+++ src/controllers/User/userController.ts
@@ -41,6 +41,7 @@ class UserController {
         userresponsebool: user.userresponsebool,
         testestresbool: user.testestresbool,
         id_empresa: user.empresa_id,
+        role_id: user.role_id, 
       });
     } catch (error) {
       console.error("Error en el login:", error);
@@ -49,40 +50,35 @@ class UserController {
   }
 
   async createUser(req: any, res: any) {
-    const { username, password, email, empresa_id } = req.body;
-    const file = req.file; // Ahora usamos req.file, ya que es un solo archivo
+    const { username, password, email, empresa_id, role_id } = req.body;
+    const file = req.file;
 
     try {
-      // Validaciones básicas
-      if (!username || !password || !email) {
+      if (!username || !password || !email || !role_id) {
         return res
           .status(400)
           .json({ error: "Todos los campos son obligatorios" });
       }
 
-      // Manejo de la imagen
       let profileImagePath = null;
       if (file) {
         const uploadDir = path.join(__dirname, "../imagenes");
         if (!fs.existsSync(uploadDir)) {
           fs.mkdirSync(uploadDir, { recursive: true });
         }
-        //nunca estaba siendo usado
-        //const fileName = `${Date.now()}-${file.filename}`;
         profileImagePath = `/imagenes/${file.filename}`;
       }
 
-      // Encriptar contraseña
       const hashedPassword = await bcrypt.hash(password, 10);
 
-      // Crear usuario
       const user = await User.create({
         username,
         password: hashedPassword,
         email,
         profileImage: profileImagePath,
         created_at: new Date(),
-        empresa_id
+        empresa_id,
+        role_id,
       });
 
       res
@@ -93,6 +89,141 @@ class UserController {
       res.status(500).json({ error: "Error interno del servidor." });
     }
   }
+
+  async getUserProfile(req: any, res: any) {
+    try {
+      const userProfile = await UserResponses.findOne({
+        where: { user_id: req.params.id },
+        include: [
+          { model: User, attributes: ["email", "profileImage", "empresa_id", "role_id"] }, 
+          { model: Hierarchical_level, attributes: ["level"] },
+        ],
+      });
+
+      if (!userProfile) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+
+      const response = {
+        email: userProfile.user.email,
+        hierarchicalLevel: userProfile.hierarchical_level.level,
+        gender_id: userProfile.gender_id,
+        profileImage: userProfile.user.profileImage,
+        id_empresa: userProfile.user.empresa_id,
+        role_id: userProfile.user.role_id, 
+      };
+
+      return res.json(response);
+    } catch (error) {
+      console.error("Error al obtener el perfil de usuario:", error);
+      return res.status(500).json({ error: "Error interno del servidor" });
+    }
+  }
+
+  async updateProfile(req: any, res: any) {
+    const { id } = req.params;
+    const { username, email, role_id } = req.body;
+
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+
+      if (username) user.username = username;
+      if (email) user.email = email;
+      if (role_id) user.role_id = role_id; 
+
+      if (req.file) {
+        const newProfileImagePath = `/imagenes/${req.file.filename}`;
+        user.profileImage = newProfileImagePath;
+      }
+
+      await user.save();
+      res
+        .status(200)
+        .json({ message: "Perfil actualizado correctamente.", data: user });
+    } catch (error) {
+      console.error("Error al actualizar el perfil:", error);
+      res.status(500).json({ error: "Error interno del servidor." });
+    }
+  }
+
+  async getAllUsers(_req: any, res: any) {
+    try {
+      const users = await User.findAll();
+      res.status(200).json(users);
+    } catch (error) {
+      console.error("Error al obtener los usuarios:", error);
+      res.status(500).json({ error: "Error al obtener los usuarios" });
+    }
+  }
+
+  async updateUser(req: any, res: any) {
+    const { id } = req.params;
+    const {
+      username,
+      email,
+      password,
+      permisopoliticas,
+      funcyinteract,
+      userresponsebool,
+      testestresbool,
+      role_id,
+    } = req.body;
+
+    try {
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+
+      if (username) user.username = username;
+      if (email) user.email = email;
+      if (password) user.password = await bcrypt.hash(password, 10);
+      if (permisopoliticas !== undefined)
+        user.permisopoliticas = permisopoliticas;
+      if (funcyinteract !== undefined) user.funcyinteract = funcyinteract;
+
+      if (userresponsebool !== undefined) {
+        user.userresponsebool =
+          userresponsebool === true || userresponsebool === "true";
+      }
+
+      if (testestresbool !== undefined) {
+        user.testestresbool =
+          testestresbool === true || testestresbool === "true";
+      }
+
+      if (role_id) user.role_id = role_id;
+
+      await user.save();
+
+      res
+        .status(200)
+        .json({ message: "Usuario actualizado correctamente", data: user });
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+      res.status(500).json({ error: "Error interno del servidor" });
+    }
+  }
+
+  async getUserById(req: any, res: any) {
+    const { id } = req.params;
+
+    try {
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(404).json({ error: "Usuario no encontrado" });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      console.error("Error al obtener el usuario:", error);
+      res.status(500).json({ error: "Error interno del servidor" });
+    }
+  }
+
   async registerBulk(req: any,res: any){
     try {
       const { empresa_id } = req.query;
@@ -191,140 +322,6 @@ class UserController {
       res.status(500).json({ message: "Error processing file", error });
     }
   }
-  // Obtener perfil de usuario
-  async getUserProfile(req: any, res: any) {
-    try {
-      const userProfile = await UserResponses.findOne({
-        where: { user_id: req.params.id },
-        include: [
-          { model: User, attributes: ["email", "profileImage", "empresa_id"] },
-          { model: Hierarchical_level, attributes: ["level"] },
-        ],
-      });
-
-      if (!userProfile) {
-        return res.status(404).json({ error: "Usuario no encontrado" });
-      }
-
-      const response = {
-        email: userProfile.user.email,
-        hierarchicalLevel: userProfile.hierarchical_level.level,
-        gender_id: userProfile.gender_id,
-        profileImage: userProfile.user.profileImage,
-        id_empresa: userProfile.user.empresa_id,
-      };
-
-      return res.json(response);
-    } catch (error) {
-      console.error("Error al obtener el perfil de usuario:", error);
-      return res.status(500).json({ error: "Error interno del servidor" });
-    }
-  }
-
-  async updateProfile(req: any, res: any) {
-    const { id } = req.params;
-    const { username, email } = req.body;
-
-    try {
-      const user = await User.findByPk(id);
-      if (!user) {
-        return res.status(404).json({ error: "Usuario no encontrado" });
-      }
-
-      if (username) user.username = username;
-      if (email) user.email = email;
-
-      // Verifica si hay una nueva imagen
-      if (req.file) {
-        //const uploadDir = path.join(__dirname, '../imagenes');
-        const newProfileImagePath = `/imagenes/${req.file.filename}`; // Usar el nombre del archivo generado por multer
-        user.profileImage = newProfileImagePath; // Actualiza la ruta de la imagen en el modelo
-      }
-
-      await user.save();
-      res
-        .status(200)
-        .json({ message: "Perfil actualizado correctamente.", data: user });
-    } catch (error) {
-      console.error("Error al actualizar el perfil:", error);
-      res.status(500).json({ error: "Error interno del servidor." });
-    }
-  }
-
-  // Obtener todos los usuarios
-  async getAllUsers(_req: any, res: any) {
-    try {
-      const users = await User.findAll();
-      res.status(200).json(users);
-    } catch (error) {
-      console.error("Error al obtener los usuarios:", error);
-      res.status(500).json({ error: "Error al obtener los usuarios" });
-    }
-  }
-
-  // Actualizar un usuario
-  async updateUser(req: any, res: any) {
-    const { id } = req.params;
-    const {
-      username,
-      email,
-      password,
-      permisopoliticas,
-      funcyinteract,
-      userresponsebool,
-      testestresbool,
-    } = req.body;
-
-    try {
-      const user = await User.findByPk(id);
-
-      if (!user) {
-        return res.status(404).json({ error: "Usuario no encontrado" });
-      }
-
-      if (username) user.username = username;
-      if (email) user.email = email;
-      if (password) user.password = await bcrypt.hash(password, 10); // Encriptar la nueva contraseña
-      if (permisopoliticas !== undefined)
-        user.permisopoliticas = permisopoliticas;
-      if (funcyinteract !== undefined) user.funcyinteract = funcyinteract;
-
-      if (userresponsebool !== undefined) {
-        // Convertir booleano a TINYINT(1) (1 o 0)
-        user.userresponsebool =
-          userresponsebool === true || userresponsebool === "true" ? true : false;
-      }
-
-      if (testestresbool !== undefined) {
-        user.testestresbool =
-          testestresbool === true || testestresbool === "true" ? true : false;
-      }
-
-      await user.save();
-
-      res
-        .status(200)
-        .json({ message: "Usuario actualizado correctamente", data: user });
-    } catch (error) {
-      console.error("Error al actualizar el usuario:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  }
-
-  // Obtener un usuario por ID
-  async getUserById(req: any, res: any) {
-    const { id } = req.params;
-
-    try {
-      const user = await User.findByPk(id);
-      if (!user) {
-        return res.status(404).json({ error: "Usuario no encontrado" });
-      }
-      res.status(200).json(user);
-    } catch (error) {
-      console.error("Error al obtener el usuario:", error);
-      res.status(500).json({ error: "Error interno del servidor" });
-    }
-  }
+  
 }
 export default new UserController();
