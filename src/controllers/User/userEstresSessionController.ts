bf079173ef18503954160209e0f93cd2561fa526
Juan Gutierrez <juan089207@gmail.com>
2025-01-21 15:26:01 -0500

    Se excluyo el estres Leve del contador

--- src/controllers/User/userEstresSessionController.ts
+++ src/controllers/User/userEstresSessionController.ts
@@ -29,23 +29,25 @@ class UserEstresSessionController{
     async assignEstresNivel(req: any, res: any){
         const { user_id, estres_nivel_id } = req.body;
     try {
+
+        console.log(req.body)
         // Busca si ya existe una sesión de estrés para el usuario
         const existingSession = await UserEstresSession.findOne({ where: { user_id } });
 
+        if (estres_nivel_id != 1){
+          const estresNivel = await EstresNiveles.findOne({
+            where: { id: estres_nivel_id }
+          });
 
-        const estresNivel = await EstresNiveles.findOne({
-          where: { id: estres_nivel_id }
-        });
+          if (!estresNivel) {
+            console.log('No se encontró el registro con el id proporcionado.');
+            res.status(500).json({ message: 'Error del servidor.' });
+            return
+          }
 
-        if (!estresNivel) {
-          console.log('No se encontró el registro con el id proporcionado.');
-          res.status(500).json({ message: 'Error del servidor.' });
-          return
+          const nuevaCantidad = estresNivel.cantidad ? estresNivel.cantidad + 1 : 1;
+          await estresNivel.update({ cantidad: nuevaCantidad });
         }
-
-        const nuevaCantidad = estresNivel.cantidad ? estresNivel.cantidad + 1 : 1;
-        await estresNivel.update({ cantidad: nuevaCantidad });
-
         if (existingSession) {
             // Si la sesión ya existe, actualiza el estres_nivel_id
             existingSession.estres_nivel_id = estres_nivel_id;
