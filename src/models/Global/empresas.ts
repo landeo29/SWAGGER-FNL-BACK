b0216f715148dfbd03b2af398b5da41e54631c2d
Agustin David Mamani Uscamayta <86539821+AdMu2838@users.noreply.github.com>
2025-01-21 09:41:17 -0500

    mejorar el controlador de usuario para incluir el rol en el inicio de sesión y agregar marcas de tiempo en el modelo de empresas

--- src/models/Global/empresas.ts
+++ src/models/Global/empresas.ts
@@ -1,4 +1,4 @@
-import { Column, DataType, Table, Model } from "sequelize-typescript";
+import { Column, DataType, Table, Model, Default } from "sequelize-typescript";
 
 @Table({
     timestamps: true,
@@ -11,4 +11,12 @@ export class Empresas extends Model {
 
     @Column(DataType.STRING)
     ruc!: string;
+
+    @Default(DataType.NOW) 
+    @Column(DataType.DATE)
+    createdAt!: Date;
+
+    @Default(DataType.NOW) 
+    @Column(DataType.DATE)
+    updatedAt!: Date;
 }
