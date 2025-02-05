4903a088dbfc14f2c544b609d1a8de4c18ed8c0c
Juan Gutierrez <juan089207@gmail.com>
2025-02-04 20:00:28 -0500

    Modificacion para el texto a voz usando Polly

--- src/controllers/Program/SpeechToTextController.ts
+++ src/controllers/Program/SpeechToTextController.ts
@@ -1,9 +1,10 @@
 import { ElevenLabsClient } from "elevenlabs";
-
+import { Polly } from 'aws-sdk';
 
 class SpeechTextController {
   private apiKey: string;
   private client: ElevenLabsClient;
+  private polly: Polly;
 
   constructor() {
     this.apiKey = process.env.ELEVENLABS_API_KEY || "";
@@ -11,9 +12,13 @@ class SpeechTextController {
       throw new Error("Falta ELEVENLABS_API_KEY en las variables de entorno");
     }
     this.client = new ElevenLabsClient({ apiKey: this.apiKey });
+
+    this.polly = new Polly({
+      region: 'us-east-1', 
+    });
   }
 
-  async generateSpeech(req: any, res: any) {
+  async generateSpeech1(req: any, res: any) {
     try {
       const { text, voiceId } = req.query;
 
@@ -61,6 +66,45 @@ class SpeechTextController {
       res.send(audioBuffer);
     } catch (error) {
       console.error("Error al generar el audio con ElevenLabs:", error);
+      res.status(500).json({ error: "Error al generar el audio" });
+    }
+  }
+
+  async generateSpeech(req: any, res: any) {
+    try {
+      const { text, voiceId } = req.query;
+
+      if (!text || typeof text !== "string") {
+        return res.status(400).json({ error: "El parámetro 'text' es obligatorio y debe ser un string." });
+      }
+      console.log(voiceId);
+      // Establecer la voz predeterminada si no se pasa una
+      const voice = "Joanna"; // Puedes cambiar la voz predeterminada aquí
+
+      // Generar el audio con Polly
+      const params: Polly.Types.SynthesizeSpeechInput = {
+        Text: text, // El texto que quieres convertir a habla
+        OutputFormat: "mp3", // El formato de salida
+        VoiceId: voice, // La voz seleccionada
+        Engine: "neural",
+      };
+
+      const data = await this.polly.synthesizeSpeech(params).promise();
+
+      if (data.AudioStream instanceof Buffer) {
+        // Configurar los headers para la respuesta
+        res.set({
+          "Content-Type": "audio/mpeg",
+          "Content-Disposition": "inline; filename=generated_audio.mp3",
+        });
+
+        // Enviar el audio generado
+        res.send(data.AudioStream);
+      } else {
+        throw new Error("Error al generar el audio");
+      }
+    } catch (error) {
+      console.error("Error al generar el audio con AWS Polly:", error);
       res.status(500).json({ error: "Error al generar el audio" });
     }
   }
